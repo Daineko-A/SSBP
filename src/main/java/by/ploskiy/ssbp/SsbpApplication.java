80240b6c1ecff5233c4b4ba6ffccac979fab09a4
Ploskiy <alexandr.daineko@gmail.com>
2022-07-26 14:46:48 +0300

    First commit.

---
+++ src/main/java/by/ploskiy/ssbp/SsbpApplication.java
@@ -0,0 +1,13 @@
+package by.ploskiy.ssbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ploskiy/ssbp/SsbpApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ploskiy.ssbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
